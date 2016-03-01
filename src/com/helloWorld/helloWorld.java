e42f322d92db7421892ade8758a406db1b9ea00a
Govinda Roul <govinda.roul.ext@konecranes.com>
2016-03-01 09:02:59 +0200

    hello world

---
+++ src/com/helloWorld/helloWorld.java
@@ -0,0 +1,11 @@
+package com.helloWorld;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HEllo World");
+	}
+
+}
